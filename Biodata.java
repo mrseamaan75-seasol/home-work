18ddefff0dfd2e654de2793a4061e84259f84342
mrseamaan75-seasol <mrseamaan75@gmail.com>
2025-10-09 22:43:42 +0530

    today new home work

---
+++ Biodata.java
@@ -0,0 +1,20 @@
+class Biodata
+{
+	public static void main(String[] args)
+	{
+		String name = "Seamaan";
+		char Initial = 'S';
+		String FatherName = "Saravana Raja Solan";
+		String MotherName = "Birundha";
+		String District = "Theni";
+		byte Date = 3;
+		byte Month = 6;
+		short Year = 2002;
+		
+		System.out.println("My Name is "+name+"."+Initial);
+		System.out.println("My Father Name is "+FatherName);
+		System.out.println("My Mother Name is "+MotherName);
+		System.out.println("My District is "+District);
+		System.out.println("Date of birth is "+Date+"/"+Month+"/"+Year);
+	}
+}
