bc0699743f7f41c5243469c72594532e37be5ef0
mrseamaan75-seasol <mrseamaan75@gmail.com>
2025-10-10 00:16:44 +0530

    my new Biodata file is upload

---
+++ BiodataNew.java
@@ -0,0 +1,29 @@
+class BiodataNew
+{
+	public static void main(String[] args)
+	{
+		String name = "Priyan";
+		char Initial = 'S';
+		String Initial1 = "MRM";
+		char Initial2 = 'S';
+		String FatherName = "Saravana Raja Solan";
+		String MotherName = "Birundha";
+		String JopName = "TNEB officer";
+		String JopName1 = "House wife";
+		String Place = "Theni";
+		int Salary = 200000;
+		byte Date = 3;
+		byte Month = 6;
+		short Year = 2002;
+		
+		System.out.println("My name is "+name+"."+Initial);
+		System.out.println("My date of birth "+Date+"/"+Month+"/"+Year);
+		System.out.println("My father name is "+FatherName+"."+Initial1);
+		System.out.println("My mother name is "+MotherName+"."+Initial2);
+		System.out.println("My father working in "+JopName);
+		System.out.println("My mother is "+JopName1);
+		System.out.println("My father annual income is "+Salary);
+		System.out.println("My home town is "+Place);
+		
+	}
+}
